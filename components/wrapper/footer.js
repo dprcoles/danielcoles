d79d4bf1b889b691008ee8cfe1a182abeba329ec
Daniel Coles <me@danielcoles.co.uk>
2021-03-26 21:07:10 +0000

    Added noreferrer to footer links

--- components/wrapper/footer.js
+++ components/wrapper/footer.js
@@ -19,6 +19,7 @@ export default function footer() {
                 className='[ Footer__Text ][ Footer__Link ] text-yellow-500 hover:text-yellow-300'
                 href={footerData.developedBy.link}
                 target='_blank'
+                rel='noreferrer'
               >
                 {footerData.developedBy.name}
               </a>
@@ -29,6 +30,7 @@ export default function footer() {
                 className='[ Footer__Text ][ Footer__Link ] text-pink-500 hover:text-pink-300'
                 href={footerData.builtIn.link}
                 target='_blank'
+                rel='noreferrer'
               >
                 {footerData.builtIn.name}
               </a>
@@ -37,6 +39,7 @@ export default function footer() {
                 className='[ Footer__Text ][ Footer__Link ] text-green-500 hover:text-green-300'
                 href={footerData.hostedOn.link}
                 target='_blank'
+                rel='noreferrer'
               >
                 {footerData.hostedOn.name}
               </a>
@@ -47,6 +50,7 @@ export default function footer() {
                 className='[ Footer__Text ][ Footer__Link ] text-indigo-500 hover:text-indigo-300'
                 href={footerData.inspiredBy.link}
                 target='_blank'
+                rel='noreferrer'
               >
                 {footerData.inspiredBy.name}
               </a>
@@ -54,7 +58,7 @@ export default function footer() {
           </div>
           <div className='grid grid-flow-row grid-cols-5 grid-rows-1'>
             <div className='[ Socials ][ Mail ] self-center justify-self-center'>
-              <a href={footerData.socials.mail}>
+              <a href={footerData.socials.mail} rel='noreferrer'>
                 <FaEnvelopeOpen
                   className='hover:text-red-400'
                   title='Get in touch by Email'
@@ -62,7 +66,11 @@ export default function footer() {
               </a>
             </div>
             <div className='[ Socials ][ Spotify ] self-center justify-self-center'>
-              <a href={footerData.socials.spotify} target='_blank'>
+              <a
+                href={footerData.socials.spotify}
+                target='_blank'
+                rel='noreferrer'
+              >
                 <FaSpotify
                   className='hover:text-green-500'
                   title='Follow me on Spotify'
@@ -70,7 +78,11 @@ export default function footer() {
               </a>
             </div>
             <div className='[ Socials ][ GitHub ] self-center justify-self-center'>
-              <a href={footerData.socials.github} target='_blank'>
+              <a
+                href={footerData.socials.github}
+                target='_blank'
+                rel='noreferrer'
+              >
                 <FaGithub
                   className='hover:text-gray-900'
                   title='Check out my GitHub'
@@ -78,7 +90,11 @@ export default function footer() {
               </a>
             </div>
             <div className='[ Socials ][ LinkedIn ] self-center justify-self-center'>
-              <a href={footerData.socials.linkedin} target='_blank'>
+              <a
+                href={footerData.socials.linkedin}
+                target='_blank'
+                rel='noreferrer'
+              >
                 <FaLinkedinIn
                   className='hover:text-blue-800'
                   title='Follow me on LinkedIn'
@@ -86,7 +102,11 @@ export default function footer() {
               </a>
             </div>
             <div className='[ Socials ][ Twitter ] self-center justify-self-center'>
-              <a href={footerData.socials.twitter} target='_blank'>
+              <a
+                href={footerData.socials.twitter}
+                target='_blank'
+                rel='noreferrer'
+              >
                 <FaTwitter
                   className='hover:text-blue-500'
                   title='Follow me on Twitter'
